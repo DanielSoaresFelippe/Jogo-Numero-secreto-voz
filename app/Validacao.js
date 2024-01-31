b576e0c20a4a56e9a49c53f0b46a6edfa24ea0a4
Daniel Soares <125377047+DanielSoaresFelippe@users.noreply.github.com>
2024-01-31 15:00:32 -0300

    Add files via upload

---
+++ app/Validacao.js
@@ -0,0 +1,40 @@
+function verificaSeOChutePossuiUmValorValido() {
+    const numero = +chute
+
+    if(chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>valor inválido</div>'
+        return
+    }
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+       elementoChute.innerHTML += `<div>valor inválido: o número secreto precisar estar entre ${menorValor} e ${maiorValor}</div>`
+
+       return
+
+    }
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou !</h2>
+        <h3> O número Secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>0 número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>0 número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
